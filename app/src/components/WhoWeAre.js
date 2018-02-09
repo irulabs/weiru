6c02c1d5bfa0838388ffd65ecdb8de2afa8701ef
sohilpandya <sohilpandya@me.com>
2018-02-09 15:36:24 +0000

    refined design

--- app/src/components/WhoWeAre.js
+++ app/src/components/WhoWeAre.js
@@ -3,7 +3,7 @@ import React from 'react';
 const WhoWeAre = () => {
   return (
     <section className="ph7-l pv5">
-      <h2>About Who We Are Us</h2>
+      <h3 className="tc">About Who We Are Us</h3>
       <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Nullam id
       dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac
       facilisis in, egestas eget quam. Aenean eu leo quam. Pellentesque ornare sem
