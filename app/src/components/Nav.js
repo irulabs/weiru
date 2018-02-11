2165d44155e292580670265eea7eb25ae361aaf4
sohilpandya <sohilpandya@me.com>
2018-02-11 13:20:43 +0000

    added styling for building eploratory layout and navbar changed related to #5

--- app/src/components/Nav.js
+++ app/src/components/Nav.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Nav = () => {
+const Nav = ({color = "bg-black", postion = "inherit"}) => {
   return (
-    <nav className="ph7-l w-100 bg-black white tl pa1 ph3">
+    <nav className={ `${postion} ph7-l w-100 ${color} white tl pa1 ph3` }>
       <h3 className="f3 ma3">iru labs</h3>
     </nav>
   )
