9ae7f4601e55546ce687b4d6f3ebba79d41876e2
sohilpandya <sohilpandya@me.com>
2018-02-12 23:09:46 +0000

    more changes related to styling related to #10

--- app/src/components/WhoWeAre.js
+++ app/src/components/WhoWeAre.js
@@ -7,7 +7,7 @@ const WhoWeAre = () => {
   return (
     <section className="ph4 ph6-m ph7-l pv5">
     { /*<h3>We build products with care</h3> */}
-      <p className="f3 fw6">
+      <p className="f3 fw6 tj">
         <span className="underline bungee f2">Care.</span> &nbsp;
         In what we work on. In how we build it. Building a product is a
         wonderful thing, but too often the people who own the idea are left
@@ -22,21 +22,21 @@ const WhoWeAre = () => {
       <div className="mt5 mt6-ns flex flex-column flex-row-ns justify-between">
         <div className="flex flex-column w-30-ns">
           <img src={crafted} className="h4" alt="Compass icon" />
-          <p className="tc">Crafted</p>
+          <h3 className="tc f3">Crafted</h3>
           <p>How we build it is important. Taking care early on means that we
           can build products which are scalable by default. As your
           userbase grows, the code we crafted for you will serve you well.</p>
         </div>
         <div className="flex flex-column w-30-ns">
           <img src={modernTech} className="h4" alt="Pacman ghost icon" />
-          <p className="tc">Modern Tech</p>
+          <h3 className="tc f3">Modern Tech</h3>
           <p>We love coding, so we&#x27;re on top of the ever-changing landscape of
           web development. We build products in the best way possible, using tech
           that is appropriate for the job.</p>
         </div>
         <div className="flex flex-column w-30-ns">
           <img src={notLockedIn} className="h4" alt="broken chains icon" />
-          <p className="tc">Not Locked In</p>
+          <h3 className="tc f3">Not Locked In</h3>
           <p>We write code for humans to understand and our
           code is tested, so if you want
           your own development team to handle maintenance, you can easily take
