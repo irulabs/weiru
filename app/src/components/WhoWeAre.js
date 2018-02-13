b76904aa95ad6be941e22ec9c20f4461a3fd1d05
sohilpandya <sohilpandya@me.com>
2018-02-13 00:05:48 +0000

    futher styling changes related to #10

--- app/src/components/WhoWeAre.js
+++ app/src/components/WhoWeAre.js
@@ -7,8 +7,8 @@ const WhoWeAre = () => {
   return (
     <section className="ph4 ph6-m ph7-l pv5">
     { /*<h3>We build products with care</h3> */}
-      <p className="f3 fw6 tj">
-        <span className="underline bungee f2">Care.</span> &nbsp;
+      <p className="f4 fw6 tj">
+        <span className="underline bungee f3">Care.  </span>
         In what we work on. In how we build it. Building a product is a
         wonderful thing, but too often the people who own the idea are left
         beind. As developers, we take you with us, working with you to define a
@@ -21,26 +21,25 @@ const WhoWeAre = () => {
 
       <div className="mt5 mt6-ns flex flex-column flex-row-ns justify-between">
         <div className="flex flex-column w-30-ns">
-          <img src={crafted} className="h4" alt="Compass icon" />
-          <h3 className="tc f3">Crafted</h3>
-          <p>How we build it is important. Taking care early on means that we
+          <img src={crafted} className="h4p5" alt="Compass icon" />
+          <h3 className="tc f4">Crafted</h3>
+          <p className=" fw6">How we build it is important. Taking care early on means that we
           can build products which are scalable by default. As your
           userbase grows, the code we crafted for you will serve you well.</p>
         </div>
         <div className="flex flex-column w-30-ns">
-          <img src={modernTech} className="h4" alt="Pacman ghost icon" />
-          <h3 className="tc f3">Modern Tech</h3>
-          <p>We love coding, so we&#x27;re on top of the ever-changing landscape of
+          <img src={modernTech} className="h4p5" alt="Pacman ghost icon" />
+          <h3 className="tc f4">Modern Tech</h3>
+          <p className="fw6">We love coding, so we&#x27;re on top of the ever-changing landscape of
           web development. We build products in the best way possible, using tech
           that is appropriate for the job.</p>
         </div>
         <div className="flex flex-column w-30-ns">
-          <img src={notLockedIn} className="h4" alt="broken chains icon" />
-          <h3 className="tc f3">Not Locked In</h3>
-          <p>We write code for humans to understand and our
-          code is tested, so if you want
-          your own development team to handle maintenance, you can easily take
-          it in-house.</p>
+          <img src={notLockedIn} className="h4p5" alt="broken chains icon" />
+          <h3 className="tc f4">Not Locked In</h3>
+          <p className="fw6">
+          Want your in-house dev team to handle maintenance? No problem: our code is tested and designed for humans to read, not just computers. We don't believe in locking people in
+          </p>
         </div>
       </div>
     </section>
