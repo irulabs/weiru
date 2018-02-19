a2e661f9d5a91a3a36eb750c5728ac85bc63e211
jessicasalmon <jessicasalmon05@gmail.com>
2018-02-19 13:21:20 +0000

    reduce boldness of card text relates #10

--- app/src/components/Card.js
+++ app/src/components/Card.js
@@ -5,13 +5,13 @@ const Card = ({ width, image, client, blurb, link }) => {
   // whole card clicks through to case study
   return (
     <div className={`${width} tl mb4 mb0-l`}>
-    <Link className="b link pointer color-inherit" to={ `/case-study/${link}` } >
+    <Link className="link pointer color-inherit" to={ `/case-study/${link}` } >
       <img alt="" src={ image } className="w-100" />
       <div className="ph1">
         <h3 className="b f4 h3 ma0 flex items-center">{ client }</h3>
         <p>{ blurb }</p>
         </div>
-        <p className="underline"> Read More </p> </Link>
+        <p className="underline b"> Read More </p> </Link>
     </div>
   )
 }
