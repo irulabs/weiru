50e25630e6e037f6ae47ecff2ea35fc1ec2742c5
jessicasalmon <jessicasalmon05@gmail.com>
2018-02-19 10:54:12 +0000

    remove a bullet point, edit text relates #10

--- app/src/components/About.js
+++ app/src/components/About.js
@@ -5,12 +5,12 @@ const About = () => {
       <section className="ph4 ph6-m ph7-l pt6 pb5 bb b--near-white bw3 bg-near-white bg-yellow">
 
       <h1 className="f1 fw6 tl dark-pink mb1 mw7-ns">
-        We build products to <span className="underline underline-yellow">delight</span> users*
+        We build products to <span className="underline underline-yellow">delight</span> users<sup className="f2">*</sup>
       </h1>
 
       <p className="dark-pink f7 mt0">*<span className="i">disclaimer</span>: there&#x27;s more to it than code.</p>
 
-      <p className="mw7-ns pt3 mid-gray near-black">
+      <p className="mw7-ns pt3 mid-gray near-black fw6">
       Donec sed odio dui. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Nullam id dolor id nibh ultricies vehicula ut id elit. Nullam quis risus eget urna mollis ornare vel eu leo.
       </p>
 
