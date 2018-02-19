870ff9fc6d73f89a1b0e14c3107d5c235037dc2d
jessicasalmon <jessicasalmon05@gmail.com>
2018-02-19 10:54:54 +0000

    remove a bullet point, edit text relates #10

--- app/src/components/OurWay.js
+++ app/src/components/OurWay.js
@@ -20,13 +20,8 @@ const OurWay = () => {
           />
 
         <CheckboxItem
-          title="Not Locked In"
-          text="Want your in-house dev team to handle maintenance? No problem: our code is tested and designed for humans to read, not just computers. We don't believe in locking people in"
-          />
-
-        <CheckboxItem
           title="Iterate by Default"
-          text="We build what&#x27;s needed first, then iterate based on data. This is only possible through the expert technologists (that's us) and the expert in the why of the app (that's you) communicating frequently and directly. That's why you'll never have to go through an account manager to talk about your product"
+          text="We build what&#x27;s needed first, then iterate based on data. This is only possible by developers and app owners communicating frequently and directly. That's why you'll never have to go through an account manager to talk about your product."
           />
       </div>
     </section>
